2ffd1a32cc92fd20bc7e28a92dd88d1be945abd9
Антон Гущин <guschin@nkmz.donetsk.ua>
2016-03-10 14:59:00 +0200

    11

--- src/main/java/servlets/MainServlet.java
+++ src/main/java/servlets/MainServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/helloSasha")
+@WebServlet("/")
 public class MainServlet extends HttpServlet {
 
     @Override
